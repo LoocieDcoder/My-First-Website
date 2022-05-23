6287415605e79e72986f918fb8ff3bef32d4cde5
Lucy <quaysonlucyofficial@gmail.com>
2022-05-24 00:00:31 +0100

    commit tree 79fa1a42f0cdcc4827c75f742ac3bee91952157e

---
+++ my practical file/.js
@@ -0,0 +1,15 @@
+function findMatching(myArray, stringName){
+
+    return myArray.filter( item => item.toLowerCase() === stringName.toLowerCase()
+    );
+  }
+  
+  function fuzzyMatch(myArray, stringName){
+  
+    return myArray.filter( item => item.indexOf(stringName) === 0 )
+  
+  }
+  
+  function matchName(driverObj, stringName){
+    return driverObj.filter(item => item.name === stringName)
+  }
